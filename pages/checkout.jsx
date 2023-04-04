f2036b99fad035bee8b689e6c6172d30f9eea20d
julio <julioroja987@gmail.com>
2023-04-03 21:58:12 -0300

    Ahora solo se puede acceder al checkout si hay items en el carrito, sino redirige al home

--- pages/checkout.jsx
+++ pages/checkout.jsx
@@ -1,42 +1,54 @@
+import { useEffect } from "react";
 import { Layout } from "@/components/layout";
 import { useAppContext } from "@/components/stateWrapper";
 import Product from "@/components/product";
 import CheckoutForm from "@/components/checkoutForm";
+import { useRouter } from "next/router";
 
 export default function Checkout() {
     const { items, getTotal } = useAppContext();
+    const router = useRouter();
 
+    useEffect(() => {
+        if (items.length === 0) {
+            router.push("/");
+        }
+    }, []);
     return (
-        <Layout>
-            <section className="min-h-[calc(100vh-340px)] px-8 sm:min-h-[calc(100vh-280px)] md:px-0">
-                <h1 className="my-6 text-base font-bold">CHECKOUT</h1>
-                <div className="flex flex-col justify-between md:flex-row">
-                    <div className="flex flex-col items-center justify-start md:w-1/2">
-                        <h2 className="mb-4 text-3xl font-bold">
-                            Complete con sus datos
-                        </h2>
+        <>
+            {items.length > 0 && (
+                <Layout>
+                    <section className="min-h-[calc(100vh-340px)] px-8 sm:min-h-[calc(100vh-280px)] md:px-0">
+                        <h1 className="my-6 text-base font-bold">CHECKOUT</h1>
+                        <div className="flex flex-col justify-between md:flex-row">
+                            <div className="flex flex-col items-center justify-start md:w-1/2">
+                                <h2 className="mb-4 text-3xl font-bold">
+                                    Complete con sus datos
+                                </h2>
 
-                        <div className="w-full">
-                            <CheckoutForm />
+                                <div className="w-full">
+                                    <CheckoutForm />
+                                </div>
+                            </div>
+                            <div className="flex max-w-[600px] flex-col pb-8 md:w-1/2">
+                                {items.map((item) => (
+                                    <Product
+                                        key={item.id}
+                                        item={item}
+                                        showAs="checkout"
+                                        quantity={item.quantity}
+                                    />
+                                ))}
+                                <div className="self-end">
+                                    <h3 className="pt-4 text-2xl font-bold">
+                                        Total: ${getTotal()}
+                                    </h3>
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                    <div className="flex max-w-[600px] flex-col pb-8 md:w-1/2">
-                        {items.map((item) => (
-                            <Product
-                                key={item.id}
-                                item={item}
-                                showAs="checkout"
-                                quantity={item.quantity}
-                            />
-                        ))}
-                        <div className="self-end">
-                            <h3 className="pt-4 text-2xl font-bold">
-                                Total: ${getTotal()}
-                            </h3>
-                        </div>
-                    </div>
-                </div>
-            </section>
-        </Layout>
+                    </section>
+                </Layout>
+            )}
+        </>
     );
 }
